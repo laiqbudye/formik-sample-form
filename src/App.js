f73e2df0f59c48da0d91d844ba9bb67f7cb79dc0
laiqb <laiqbudye@gmail.com>
2021-11-19 17:16:50 +0530

    YUP || added form validations using Yup library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import YouTubeForm from './components/YouTubeForm';
+import NewYouTubeForm from './components/NewYouTubeForm';
 
 function App() {
   return (
     <div className="App">
-      <YouTubeForm />
+      {/* <YouTubeForm /> */}
+      <NewYouTubeForm />
     </div>
   );
 }
